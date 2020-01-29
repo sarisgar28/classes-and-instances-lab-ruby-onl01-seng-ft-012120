f33113ac2405c475f1017c7ea29fa7c6f8b35053
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-29 20:33:27 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+def Dog_Class(name)
+def dog.new = {"fido","snoopy","lassie"}
+end 
+
+def Dog_class=(name)
+@fido= [Dog.new] 
+@snoopy= [Dog.new]
+@assie = [Dog.new]
+end 
---
+++ lib/person.rb
@@ -0,0 +1,5 @@
+Person class=(name)
+def people_you_have_to_know_of (name)
+  @person #{adele_goldberg)
+  @person #{alan_kay)
+end 
